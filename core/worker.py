Feat: fix the numerical issue in multi gpu engine profiling.

core/worker.py
@@ -51,26 +51,24 @@ def worker(start_time, rank, world_size, shared_batch_size, shared_array, barrie
             case "Profile":
                 pipeline.init_profile_data()
 
+                # stream_names = ["TEST_TOTAL"]
                 stream_names = [ f"TEST_{i}" for i in range(len(pipeline.sm_counts)) ] + ["TEST_TOTAL"]
-                # stream_names = [ "TEST_1" ]  # For testing purposes, we only use one stream.
                 for stream_name in stream_names:
-                    # pipeline.reset()
-                    # print(f"Stream: {stream_name}")
+                    pipeline.reset()
+                    print(f"Stream: {stream_name}")
 
-                    # # test for prefill
-                    # # total_batch_sizes = [128, 256, 384, 512, 640, 768, 896, 1024]
-                    # # total_batch_sizes = [128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280, 1408, 1536, 1664, 1792, 1920, 2048]
+                    # test for prefill
+                    total_batch_sizes = [128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280, 1408, 1536, 1664, 1792, 1920, 2048]
                     # total_batch_sizes = [2048]
-                    # for idx, total_batch_size in enumerate(total_batch_sizes):
-                    #     input = [(idx, input_ids[:total_batch_size].copy())]
+                    for idx, total_batch_size in enumerate(total_batch_sizes):
+                        input = [(idx, input_ids[:total_batch_size].copy())]
 
-                    #     pipeline.update(input, is_profile=True, stream_name=stream_name)
-                    #     pipeline.profile_run()
+                        pipeline.update(input, is_profile=True, stream_name=stream_name)
+                        pipeline.profile_run()
                 
                     # test for decode
-                    # total_batch_sizes = [128, 256, 384]
-                    # total_batch_sizes = [128, 256, 384, 512, 640, 768, 896, 1024]
-                    total_batch_sizes = [2]
+                    total_batch_sizes = [128, 256, 384]
+                    # total_batch_sizes = [384]
                     # prepare the decode inputs for a special input_length
                     input_length = 1024
                     # output_length = 0
@@ -82,20 +80,15 @@ def worker(start_time, rank, world_size, shared_batch_size, shared_array, barrie
                         # initialize the reqs for first {total_batch_size} requests
                         for i in range(total_batch_size):
                             input = [(i, prefill_input_ids.copy())]
-                            pipeline.update(input, is_profile=True)
+                            pipeline.update(input)
                             new_tokens = pipeline.run()
                             decode_inputs.extend(new_tokens)
                             print("new_tokens: ", new_tokens)
                             print("Stream_name: ", stream_name)
                             print("total_batch_size: ", total_batch_size)
 
-                        # torch.cuda.synchronize()
-                        # # decode profiling from input_length to input_length + output_length
-                        # # for i in range(output_length + 1):
-                        # #     print("Cycle: ", i)
-                        # pipeline.update(decode_inputs, total_batch_size, is_profile=True, stream_name=stream_name)
-                        # # if i % 128 == 0:
-                        # pipeline.profile_run()
+                        pipeline.update(decode_inputs, total_batch_size, is_profile=True, stream_name=stream_name)
+                        pipeline.profile_run()
 
             case "Terminate":
                 # Termination signal received.